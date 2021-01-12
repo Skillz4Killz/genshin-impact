cac5c7beff7ab6e9491f8011abc08cb5741b4251
Skillz <gankstarsleague@gmail.com>
2021-01-12 12:11:24 -0500

    schemas

--- src/database/schemas.ts
+++ src/database/schemas.ts
@@ -1,18 +1,33 @@
 export interface ClientSchema {
-	/** The bot id */
+  /** The bot id */
   id: string;
 }
 
 export interface GuildSchema {
-	/** The guild id */
-	id: string;
-	/** The custom prefix for this guild */
-	prefix: string;
-	/** The language for this guild */
+  /** The guild id */
+  id: string;
+  /** The custom prefix for this guild */
+  prefix: string;
+  /** The language for this guild */
   language: string;
 }
 
 export interface UserSchema {
   /** The user id who created this emoji */
   id: string;
+  /** The users uid. May be Not Available if user skipped. */
+  uid: string;
+  /** The characters that the user owns */
+  characters: OwnedCharacter[];
+  /** The adventurer rank */
+  adventurerRank: number;
+  /** The world levelfor the user */
+  worldLevel: number;
+}
+
+export interface OwnedCharacter {
+  /** The name of the character */
+  name: string;
+  /** The level of this character constellation */
+  constellationLevel: number;
 }
