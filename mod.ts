4b651095e6157bac5b8a15b69b3e7f77431634fe
Skillz <gankstarsleague@gmail.com>
2021-01-12 12:11:07 -0500

    intents

--- mod.ts
+++ mod.ts
@@ -3,9 +3,7 @@ import { configs } from "./configs.ts";
 import { importDirectory } from "./src/utils/helpers.ts";
 import { loadLanguages } from "./src/utils/i18next.ts";
 
-console.info(
-  "Beginning Bot Startup Process. This can take a little bit depending on your system. Loading now...",
-);
+console.info("Beginning Bot Startup Process. This can take a little bit depending on your system. Loading now...");
 
 // Forces deno to read all the files which will fill the commands/inhibitors cache etc.
 await Promise.all(
@@ -18,9 +16,7 @@ await Promise.all(
     "./src/tasks",
     "./src/permissionLevels",
     "./src/events",
-  ].map(
-    (path) => importDirectory(Deno.realPathSync(path)),
-  ),
+  ].map((path) => importDirectory(Deno.realPathSync(path)))
 );
 
 // Loads languages
@@ -31,7 +27,7 @@ startBot({
   token: configs.token,
   // Pick the intents you wish to have for your bot.
   // For instance, to work with guild message reactions, you will have to pass the Intents.GUILD_MESSAGE_REACTIONS intent to the array.
-  intents: [Intents.GUILDS, Intents.GUILD_MESSAGES],
+  intents: [Intents.GUILDS, Intents.GUILD_MESSAGES, Intents.DIRECT_MESSAGES, Intents.DIRECT_MESSAGE_REACTIONS],
   // These are all your event handler functions. Imported from the events folder
   eventHandlers: botCache.eventHandlers,
 });
