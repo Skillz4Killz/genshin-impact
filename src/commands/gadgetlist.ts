2d221abb2e7d734f0a54b414b96105071527457c
GeheimerWolf <PatrickBruederle@gmail.com>
2021-01-18 23:32:08 +0100

    fix

--- src/commands/gadgetlist.ts
+++ src/commands/gadgetlist.ts
@@ -13,13 +13,13 @@ createCommand({
     const embed = new Embed()
       .setTitle("Gadgetlist")
       .setDescription([
-        `*⭐⭐⭐`,
+        `⭐⭐⭐`,
         "Geoculus Resonance Stone 🔸 Anemoculus Resonance Stone 🔸 Adepti Seeker's Stove 🔸 Warming Bottle",
         "",
-        `*⭐⭐⭐⭐`,
+        `⭐⭐⭐⭐`,
         "Condensed Resin 🔸 Wind Catcher 🔸 NRE (Menu 30) 🔸 Geo Treasure Compass 🔸 Anemo Treasure Compass 🔸 Portable Waypoint 🔸 Kamera 🔸 Mini Seelie: Curcuma 🔸 Mini Seelie: Dayflower 🔸 Mini Seelie: Rosé 🔸 Treasure-Seeking Seelie",
         "",
-        "If you want to search for gadget infos, type `p!gadget (name)`\nThe gadgetname has to be lower case and without spacing. For example `p!gadget windcatcher`",
+        "If you want to search for gadget infos, type `p!gadget (name)`\nThe gadget name has to be lower case and without spacing. For example `p!gadget windcatcher`",
       ])
 
       await sendEmbed(message.channelID, embed).catch(console.log);
