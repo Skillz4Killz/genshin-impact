0660005ce9c4bb987431ca659a09fbf1fa17c6ff
GeheimerWolf <PatrickBruederle@gmail.com>
2021-01-21 22:29:34 +0100

    test

--- src/commands/acension gems/brilliantdiamond.ts
+++ src/commands/acension gems/brilliantdiamond.ts
@@ -1,73 +1,106 @@
-import { botCache, Message } from "../../../deps.ts";
+import { addReactions, Message, removeUserReaction } from "../../../deps.ts";
 import { needReaction } from "../../utils/collectors.ts";
 import { Embed } from "../../utils/Embed.ts";
-import { createCommand } from "../../utils/helpers.ts";
+import { createCommand, editEmbed, sendEmbed } from "../../utils/helpers.ts";
 
 createCommand({
   name: "brilliantdiamond",
   aliases: ["bd"],
   arguments: [
-    { name: "page", type: "number", defaultValue: 1 },
+    { name: "page", defaultValue: 1 },
   ],
   guildOnly: true,
-  execute: async function (message, args) {
+  execute: async function (message) {
 
-      const first = new Embed()
+      const firstEmbed = new Embed()
       .setTitle("Rarity ⭐⭐")
       .setDescription([
-        "",
+        "1",
       ])
 
-    const second = new Embed()
+    const secondEmbed = new Embed()
       .setTitle("Rarity ⭐⭐⭐")
       .setDescription([
-        "",
+        "2",
       ])
 
-      const third = new Embed()
+      const thirdEmbed = new Embed()
       .setTitle("Rarity ⭐⭐⭐⭐")
       .setDescription([
-        "",
+        "3",
       ])
 
-      const fourth = new Embed()
+      const fourthEmbed = new Embed()
       .setTitle("Rarity ⭐⭐⭐⭐⭐")
       .setDescription([
-        "",
+        "4",
       ])
 
-
-    const pages = {
-      1: { page: 1, embed: first, emoji: "1️⃣" },
-      2: { page: 2, embed: second, emoji: "2️⃣" },
-      3: { page: 3, embed: third, emoji: "3️⃣" },
-      4: { page: 4, embed: fourth, emoji: "4️⃣" },
-    } as Record<number, { page: number; embed: Embed; emoji: string } | undefined>;
-
-    const page = pages[args.page];
-    if (!page) return;
-
-    // SEND FIRST EMBED
-    const response = args.msg
-      ? await (args.msg as Message).edit({ embed: page.embed }).catch(console.log)
-      : await message.reply({ embed: page.embed }).catch(console.log);
-    if (!response) return;
-
-    const emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣"];
-    // ADD THE REACTIONS
-    if (!args.msg) await response.addReactions(emojis, true).catch(console.log);
-
-    // HANDLE PAGINATION
-    const reaction = await needReaction(message.author.id, response.id, {
-      filter: (userID, reaction) => message.author.id === userID && page.emoji !== reaction,
-    }).catch(console.log);
-    if (!reaction) return;
-
-    const selectedPage = Object.values(pages).find((page) => page?.emoji === reaction);
-    if (!selectedPage) return;
-
-    return botCache.commands
-    .get("brilliantdiamond")
-    ?.execute?.(message, { character: args.character, page: selectedPage.page, msg: response });
+      createPagination(message, [firstEmbed,secondEmbed,thirdEmbed,fourthEmbed])
 },
 });
+
+export async function createPagination(message: Message, embeds: Embed[], page = 1 ): Promise<void> {
+    if (embeds.length === 0)
+        return;
+
+    const {channelID, author} = message;
+
+    let currentPage: number = page - 1;
+
+    let embedMessage = await sendEmbed(channelID, embeds[currentPage]);
+
+    if (!embedMessage)
+        return;
+
+    if (embeds.length <= 1)
+        return;
+
+    try {
+        addReactions(
+            embedMessage.channelID,
+            embedMessage.id,
+            ['⏮️','◀️','▶️','⏭️'],
+            true
+        )
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+
+    while(true) {
+        if (!embedMessage)
+            return;
+        const reaction = await needReaction(author.id, embedMessage.id);
+
+        if (!reaction)
+            return;
+
+        if(!(removeUserReaction(message.channelID, embedMessage.id, reaction, message.author.id).catch(console.error)))
+            return;
+
+        if (reaction === '◀️') {
+            currentPage--;
+        } else if (reaction === '▶️') {
+            currentPage++;
+        } else if (reaction === '⏮️') {
+            currentPage = 0;
+        } else if (reaction === '⏭️') {
+            currentPage = embeds.length - 1;
+        } else {
+            continue;
+        }
+
+        if (currentPage < 0)
+            currentPage = 0;
+
+        if (currentPage > embeds.length - 1)
+            currentPage = embeds.length - 1;
+
+        if (!embedMessage)
+            return;
+
+        if(!(await editEmbed(embedMessage, embeds[currentPage]).catch(console.error)))
+            return;
+    }
+}
