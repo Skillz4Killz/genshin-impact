7c72e75625a3f6e74990ff63058fff26a4b910ae
GeheimerWolf <PatrickBruederle@gmail.com>
2021-01-23 21:06:55 +0100

    reeeee

--- src/commands/lists/listmaterials.ts
+++ src/commands/lists/listmaterials.ts
@@ -194,7 +194,7 @@ createSubcommand("list", {
       const selectedPage = Object.values(pages).find((page) => page?.emoji === reaction);
       if (!selectedPage) return;
       return botCache.commands
-      .get("materiallist")
+      .get("listmaterials")
       ?.execute?.(message, { character: args.character, page: selectedPage.page, msg: response });
   },
   });
