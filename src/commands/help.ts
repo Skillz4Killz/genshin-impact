59672e2c5c5a872fe9f811992638b1f7614daa3a
GeheimerWolf <PatrickBruederle@gmail.com>
2021-01-24 19:59:10 +0100

    fGÄlhj

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,5 @@
 import { Embed } from "../utils/Embed.ts";
-import { createCommand, createSubcommand, sendEmbed } from "../utils/helpers.ts";
+import { createCommand, createSubcommand } from "../utils/helpers.ts";
 
 createCommand({
   name: `help`,
@@ -24,44 +24,3 @@ createCommand({
 },
 }); 
 
-createSubcommand("help", {
-  name: "materials",
-  aliases: ["material"],
-  guildOnly: true,
-  execute: async function (message, args) {
-
-    const embed = new Embed()
-      .setTitle("Help Material")
-      .setDescription([
-        "**Ascension Gems**",
-        "🔹 `p!material [agnidusagate | agndius | aa]`",
-        "🔹 `p!material [brilliantdiamond | diamond | bd]`",
-        "🔹 `p!material [prithivatopaz | prithi | pv]`",
-        "🔹 `p!material [vajradaamethyst | vajrada | va]`",
-        "🔹 `p!material [varunadalazurite | varunada | vl]`",
-        "🔹 `p!material [vayudaturquoise | vayuda | vt]`",
-        "",
-        "**Talent Level-Up Materials**",
-        "🔹 `p!material [ballad | teachingsofballad | guidetoballad | philosophiesofballad]`",
-        "🔹 `p!material [diligence | teachingsofdiligence | guidetodiligence | philosophiesofdiligence]`",
-        "🔹 `p!material [freedom | teachingsoffreedom | guidetofreedom | philosophiesoffreedom]`",
-        "🔹 `p!material [gold | teachingsofgold | guidetogold | philosophiesofgold]`",
-        "🔹 `p!material [prosperity | teachingsofprosperity | guidetoprosperity | philosophiesofprosperity]`",
-        "🔹 `p!material [resistance | teachingsofresistance | guidetoresistance | philosophiesofresistance]`",
-        "",
-        "🔹 `p!material crownofinsight`",
-        "🔹 `p!material dvalinsclaw`",
-        "🔹 `p!material dvalinsplume`",
-        "🔹 `p!material dvalinssigh`",
-        "🔹 `p!material ringofboreas`",
-        "🔹 `p!material shadowofthewarriors`",
-        "🔹 `p!material [shardofafoullegacy | foullegacy]`",
-        "🔹 `p!material [spiritlocketofboreas | spiritlocket]`",
-        "🔹 `p!material [tuskofmonoceroscaeli | tuskofmonoceros | monoceroscaeli | monoceros]`"
-      ])
-      .setColor("RANDOM")
-
-      await sendEmbed(message.channelID, embed).catch(console.log);
-
-  }
-});
