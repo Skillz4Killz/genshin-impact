36cb00634ffe5a41587f30ad23c698d286721edb
GeheimerWolf <PatrickBruederle@gmail.com>
2021-01-24 19:36:59 +0100

    fml

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,5 +1,5 @@
 import { Embed } from "../utils/Embed.ts";
-import { createCommand, createSubcommand, sendEmbed } from "../utils/helpers.ts";
+import { createCommand } from "../utils/helpers.ts";
 
 createCommand({
   name: `help`,
@@ -17,23 +17,9 @@ createCommand({
         "",
         "To access the command list, please use the command `p!commands`!",
         "",
-        "Need further help? Visit our Support Server!",])
+        "Need further help? Visit our Support Server!"
+       ])
       })
-    }
-
-    if (!createCommand) {
-      return message.reply({content: "https://discord.gg/26MjArxVP3", embed: new Embed()
-      .setTitle("Need help, Traveler?")
-      .setDescription([
-        "To set up your profile run `p!setup` and go to my DMs!",
-        "",
-        "To check your profile, run the command `p!profile`!",
-        "",
-        "To access the command list, please use the command `p!commands`!",
-        "",
-        "Need further help? Visit our Support Server!",])
-      })
-
-  }
-},
+    
+  },
 });
