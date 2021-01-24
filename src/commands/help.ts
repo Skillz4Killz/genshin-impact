e325217d83f71a6e7d26bea0e1f83e5f8bc4a7db
GeheimerWolf <PatrickBruederle@gmail.com>
2021-01-24 19:30:12 +0100

    dunno what im doing

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -20,5 +20,20 @@ createCommand({
         "Need further help? Visit our Support Server!",])
       })
     }
-  },
+
+    if (!createCommand) {
+      return message.reply({content: "https://discord.gg/26MjArxVP3", embed: new Embed()
+      .setTitle("Need help, Traveler?")
+      .setDescription([
+        "To set up your profile run `p!setup` and go to my DMs!",
+        "",
+        "To check your profile, run the command `p!profile`!",
+        "",
+        "To access the command list, please use the command `p!commands`!",
+        "",
+        "Need further help? Visit our Support Server!",])
+      })
+
+  }
+},
 });
