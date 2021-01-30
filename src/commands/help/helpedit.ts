9e4a3b9166f8a47dda695aa4a4c1790fedbcf359
GeheimerWolf <PatrickBruederle@gmail.com>
2021-01-30 12:45:46 +0100

    dfdd

--- src/commands/help/helpedit.ts
+++ src/commands/help/helpedit.ts
@@ -3,16 +3,21 @@ import { createSubcommand, sendEmbed } from "../../utils/helpers.ts";
 
 createSubcommand("help", {
   name: "edit",
+  aliases: ["add"],
   guildOnly: true,
   execute: async function (message) {
     const embed = new Embed()
       .setTitle("Help Edit")
       .setDescription([
-        "🔹 `p!edit uid (new UID)`",
+        "🔹 `p!edit uid (UID)`",
         "🔹 `p!edit [ar | adventurerrank] (1-60)`",
         "🔹 `p!edit [wl | worldlevel] (0-8)`",
         "🔹 `p!edit [char | character] (char name) (constellation 0-6)`",
-        "With editing a character you're also able to add a new character.",
+        "",
+        "🔹 `p!add uid (UID)`",
+        "🔹 `p!add [ar | adventurerrank] (1-60)`",
+        "🔹 `p!add [wl | worldlevel] (0-8)`",
+        "🔹 `p!add [char | character] (char name) (constellation 0-6)`",
       ])
       .setColor("RANDOM");
 
