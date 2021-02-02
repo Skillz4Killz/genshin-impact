d69801c523831c40cb8e0a68cc6784bd8f1f6200
GeheimerWolf <PatrickBruederle@gmail.com>
2021-02-02 22:47:36 +0100

    d

--- src/commands/infomaterial.ts
+++ src/commands/infomaterial.ts
@@ -1,6 +1,19 @@
+import { Embed } from "../utils/Embed.ts";
 import { createCommand } from "../utils/helpers.ts";
 
 createCommand({
   name: "material",
-  arguments: [{ name: "subcommand", type: "subcommand" }],
+  arguments: [{ name: "subcommand", type: "subcommand", required: false }],
+  guildOnly: true,
+  execute: async function (message) {
+    await message.reply({
+      embed: new Embed()
+        .setTitle("Need help, Traveler?")
+        .setDescription([
+          "You forgot to provide the name of a material.",
+          "",
+          "If you need help, try `p!help materials`",
+        ]),
+    }).catch(console.log);
+  },
 });
