73439008ce7e7f10e1fdc7beac768459d46a7d87
GeheimerWolf <PatrickBruederle@gmail.com>
2021-02-16 08:16:04 +0100

    dd

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,7 +1,44 @@
-import { botCache } from "../../deps.ts";
+import { configs } from "../../configs.ts";
+import { botCache, botID, sendMessage } from "../../deps.ts";
+import { Embed } from "../utils/Embed.ts";
 
-botCache.eventHandlers.guildCreate = function (guild) {
+botCache.eventHandlers.guildCreate = async (guild) => {
   console.info(
     `[EVENT=GuildCreate]: ${guild.name} with ${guild.memberCount} members.`,
   );
+
+  const embed = new Embed().setColor("06ac00").setTitle("NEW SERVER ADDED")
+    .addField("Name", guild.name, true)
+    .addField("ID", guild.id, true)
+    .addField(
+      "Members",
+      botCache.helpers.cleanNumber(guild.memberCount),
+      true,
+    )
+    .addField("Shard ID", `${guild.shardID}`)
+    .setTimestamp();
+
+  await sendMessage(configs.channelIDs.serverStats, { embed }).catch(
+    console.log,
+  );
+
+
+botCache.eventHandlers.guildDelete = async (guild) => {
+  console.info(
+    `[EVENT=GuildDelete]: ${guild.name} with ${guild.memberCount} members.`,
+  );
+
+  const embed = new Embed().setColor("ff0000").setTitle("SERVER REMOVED")
+    .addField("Name", guild.name, true)
+    .addField("ID", guild.id, true)
+    .addField(
+      "Members",
+      botCache.helpers.cleanNumber(guild.memberCount),
+      true,
+    ).setTimestamp();
+
+  await sendMessage(configs.channelIDs.serverStats, { embed }).catch(
+    console.log,
+  );
 };
+}
