6b2c7a9d92dc60a5063fe2d452566181651fab4f
GeheimerWolf <PatrickBruederle@gmail.com>
2021-02-21 18:01:56 +0100

    less logs

--- mod.ts
+++ mod.ts
@@ -8,7 +8,7 @@ console.info(
 );
 
 // Forces deno to read all the files which will fill the commands/inhibitors cache etc.
-console.log(await Promise.all(
+await Promise.all(
   [
     "./src/commands",
     "./src/inhibitors",
@@ -20,15 +20,15 @@ console.log(await Promise.all(
     "./src/permissionLevels",
     "./src/events",
   ].map((path) => importDirectory(Deno.realPathSync(path))),
-));
+);
 // Loads languages
-console.log(await loadLanguages());
-console.log(await import("./src/database/database.ts"));
-console.log(await importDirectory(Deno.realPathSync("./src/constants")));
-console.log(await importDirectory(Deno.realPathSync("./src/helpers")));
-console.log(await importDirectory(Deno.realPathSync("./src/events")));
+await loadLanguages();
+await import("./src/database/database.ts");
+await importDirectory(Deno.realPathSync("./src/constants"));
+await importDirectory(Deno.realPathSync("./src/helpers"));
+await importDirectory(Deno.realPathSync("./src/events"));
 
-console.log(startBot({
+startBot({
   token: configs.token,
   // Pick the intents you wish to have for your bot.
   // For instance, to work with guild message reactions, you will have to pass the Intents.GUILD_MESSAGE_REACTIONS intent to the array.
@@ -41,4 +41,4 @@ console.log(startBot({
   ],
   // These are all your event handler functions. Imported from the events folder
   eventHandlers: botCache.eventHandlers,
-}));
+});
