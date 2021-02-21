6c6ed24cf892ed5552f485b9ed01e697d06c5ef9
GeheimerWolf <PatrickBruederle@gmail.com>
2021-02-21 17:05:22 +0100

    test

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,7 @@
-import { ActivityType, botCache, cache } from "../../deps.ts";
+import { botCache, ActivityType, cache } from "../../deps.ts";
 import { wolfsStatusFunction } from "../utils/status.ts";
 import { registerTasks } from "./../utils/taskHelper.ts";
+
 
 botCache.eventHandlers.ready = function () {
   wolfsStatusFunction({
@@ -12,6 +13,15 @@ botCache.eventHandlers.ready = function () {
     },
   });
 
+  //wolfsStatusFunction({
+  //  status: "online",
+  //  activity: {
+  //    type: ActivityType.Streaming,
+  //    name: "p!setup to setup profile",
+  //    url: "https://www.twitch.tv/superboss4theultimatehero",
+  //  },
+  //});
+
   console.log(`Loaded ${botCache.arguments.size} Argument(s)`);
   console.log(`Loaded ${botCache.commands.size} Command(s)`);
   console.log(`Loaded ${Object.keys(botCache.eventHandlers).length} Event(s)`);
@@ -20,9 +30,7 @@ botCache.eventHandlers.ready = function () {
   console.log(`Loaded ${botCache.tasks.size} Task(s)`);
 
   registerTasks();
-
   botCache.fullyReady = true;
-
   console.log(
     `[READY] Bot is online and ready in ${cache.guilds.size} guild(s)!`,
   );
