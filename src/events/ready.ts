915f5d195c4866bd68141b7729cbcdc738bfa5a8
GeheimerWolf <PatrickBruederle@gmail.com>
2021-02-21 17:07:12 +0100

    d

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,26 +1,18 @@
-import { botCache, ActivityType, cache } from "../../deps.ts";
-import { wolfsStatusFunction } from "../utils/status.ts";
+import {
+  ActivityType,
+  botCache,
+  cache,
+  editBotsStatus,
+  StatusTypes,
+} from "../../deps.ts";
 import { registerTasks } from "./../utils/taskHelper.ts";
 
-
 botCache.eventHandlers.ready = function () {
-  wolfsStatusFunction({
-    status: "online",
-    activity: {
-      type: ActivityType.Streaming,
-      name: "p!setup to setup profile",
-      url: "https://www.twitch.tv/superboss4theultimatehero",
-    },
-  });
-
-  //wolfsStatusFunction({
-  //  status: "online",
-  //  activity: {
-  //    type: ActivityType.Streaming,
-  //    name: "p!setup to setup profile",
-  //    url: "https://www.twitch.tv/superboss4theultimatehero",
-  //  },
-  //});
+  editBotsStatus(
+    StatusTypes.DoNotDisturb,
+    "Discordeno Best Lib",
+    ActivityType.Game,
+  );
 
   console.log(`Loaded ${botCache.arguments.size} Argument(s)`);
   console.log(`Loaded ${botCache.commands.size} Command(s)`);
@@ -30,7 +22,7 @@ botCache.eventHandlers.ready = function () {
   console.log(`Loaded ${botCache.tasks.size} Task(s)`);
 
   registerTasks();
-  botCache.fullyReady = true;
+
   console.log(
     `[READY] Bot is online and ready in ${cache.guilds.size} guild(s)!`,
   );
