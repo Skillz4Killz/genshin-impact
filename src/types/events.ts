ceed71140c340bd5ab874fec1812ccbed5a9e199
GeheimerWolf <PatrickBruederle@gmail.com>
2021-02-22 18:01:47 +0100

    test

--- src/types/events.ts
+++ src/types/events.ts
@@ -2,7 +2,4 @@ import type { EventHandlers } from "../../deps.ts";
 
 // This interface is a placeholder that allows you to easily add on custom events for your need.
 export interface CustomEvents extends EventHandlers {
-    commandsSent: (message: any) => Promise<void>;
-    messageSent: (message: any) => Promise<void>;
-    messageEdit: (message: any) => Promise<void>;
 }
