1676096185c4f5dca0a504624c6af840461ff833
GeheimerWolf <PatrickBruederle@gmail.com>
2021-02-23 19:37:20 +0100

    prefix

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,8 @@
-import { Sabr, SabrTable } from "../../deps.ts";
+import { configs } from "../../configs.ts";
+import { botCache, Sabr, SabrTable } from "../../deps.ts";
 import { ClientStatsSchema, GuildSchema, UserSchema } from "./schemas.ts";
+
+
 
 // Create the database class
 const sabr = new Sabr();
@@ -12,6 +15,14 @@ export const db = {
   clientstats: new SabrTable<ClientStatsSchema>(sabr, "clientstats"),
 
 };
+
+const [guildSettings] = await Promise.all([db.guilds.getAll(true)])
+
+for (const settings of guildSettings) {
+  if (settings.prefix !== configs.prefix) {
+    botCache.guildPrefixes.set(settings.id, settings.prefix)
+  }
+}
 
 // This is important as it prepares all the tables.
 await sabr.init();
