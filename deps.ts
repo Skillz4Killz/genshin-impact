f5e8528f0d4c2db755bada15750eddb1372ffb85
GeheimerWolf <PatrickBruederle@gmail.com>
2021-03-02 21:18:28 +0100

    deps.ts fix

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export * from "https://deno.land/std@0.85.0/fmt/colors.ts";
 export { fromFileUrl, toFileUrl } from "https://deno.land/std@0.85.0/path/mod.ts";
 export * as confusables from "https://deno.land/x/discordeno@10.3.0/mod.ts";
-export * from "https://github.com/discordeno/discordeno/tree/10.4.0";
+export * from "https://deno.land/x/discordeno@10.4.0/mod.ts";
 export { Image } from "https://raw.githubusercontent.com/matmen/ImageScript/deno/mod.ts";
 export * from "https://deno.land/x/sabr@1.1.2/mod.ts";
 export * from "./cache.ts";
