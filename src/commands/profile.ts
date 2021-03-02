bd2ac7b4403bd4e6c9a382fbe18306de33838ffe
GeheimerWolf <PatrickBruederle@gmail.com>
2021-03-02 21:39:33 +0100

    itoh

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -80,9 +80,9 @@ createCommand({
             const char = characters.get(character.name.toLowerCase());
             // NOT A CHAR IN CONSTANTS
             if (!char) return "";
-            console.log(character)
             // NOT FOR THIS CATEGORY
             if (char.category !== category.name) return "";
+            console.log(character)
             // CHAR DETAILS
             return `${char ? `${char.emoji} ` : ""}${character.name} *(Lv. ${character.charLevel} • C${character.constellationLevel})*`;
           })
